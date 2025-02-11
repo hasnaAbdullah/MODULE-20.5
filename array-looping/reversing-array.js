657ee57450d828ac6748e85aa9ff61e86a3bc2b6
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-11 19:15:08 +0600

    I have tested reversing array using basic loop and array methods

--- array-looping/reversing-array.js
+++ array-looping/reversing-array.js
@@ -1,1 +1,18 @@
 const friends = ["Sajid", "Dihan", "Mohammed", "Tanisha", "Jihad"];
+let i ; 
+let totalFriends = friends.length - 1;
+const newFriendsList = [];
+// for(const friend of friends) {
+//     newFriendsList.unshift(friend);
+// }
+// ...................................... /
+// while (i < totalFriends) {
+//   newFriendsList.unshift(friends[i]);
+//   i++;
+// }
+// .......................................... /
+
+for (i = totalFriends ; i >= 0; i--) {
+  newFriendsList.push(friends[i]);
+}
+console.log(newFriendsList);
