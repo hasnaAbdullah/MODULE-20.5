8caaa60dd16edcdf57e9f28e1bf1fc19951f1384
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-12 09:19:32 +0600

    I have done task 01

---
+++ array-task/task-01.js
@@ -0,0 +1,11 @@
+/* 
+Task 01. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+const fruits = ["apple", "orange", "mango", "banana", "watermelon"];
+console.log(fruits[[3]]);
+fruits[2] = "jambura";
+console.log(fruits);
