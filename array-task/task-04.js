5dee86a4543d4c58f1123b8664470ce64bd1d2eb
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-12 09:39:44 +0600

    I have done task 4

---
+++ array-task/task-04.js
@@ -0,0 +1,19 @@
+/*
+Task 04. Checking if it's an Array
+Instructions:
+
+Create different variables, each containing either an array or a non-array value.
+
+Now use isArray to check if each variable is an array.
+
+Print a message to the console indicating whether each variable is an array or not.
+*/
+const book = "javascript";
+const fruit = "mango";
+const color = ["yellow"];
+const age = 25;
+if (Array.isArray(book)) {
+  console.log("yes, it's an array");
+} else {
+  console.log("no, it's not an array");
+}
