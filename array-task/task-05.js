ddf16a280b8d75134dfc3bddf166604f8239de0c
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-12 09:43:55 +0600

    I have done task 5

---
+++ array-task/task-05.js
@@ -0,0 +1,15 @@
+/*
+Task 05. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+*/
+
+const friends = ["sajid", "mohammed", "dihan"];
+const newFriends = ["jihad", "atik"];
+let allFriends = friends.concat(newFriends);
+console.log(friends);
+console.log(newFriends);
+console.log(allFriends);
