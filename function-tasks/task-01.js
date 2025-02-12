c6f23f7fea6f4bca9a134009267765923aa79399
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-13 02:10:09 +0600

    task 01 is done

---
+++ function-tasks/task-01.js
@@ -0,0 +1,6 @@
+function multiplier(num1, num2, num3, num4) {
+  let result = num1 * num2 * num3 * num4;
+  return result;
+}
+let resultOfMultiply = multiplier(2, 3, 4, 5);
+console.log(resultOfMultiply)
