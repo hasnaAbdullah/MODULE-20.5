75f009ba89fa83a5fcd0018342abed2db06b55c7
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-17 19:26:14 +0600

    problem 01 is done

---
+++ B11-A-04-tasks/problem-01.js
@@ -0,0 +1,8 @@
+function  calculateVAT(price) {
+    if(typeof price !== 'number' || price < 0) {
+        return 'Invalid';
+    }
+    let rate = 7.5 / 100;
+    let VAT = price * rate;
+    return VAT;
+}
